3303c46e2286540bd6a7023fabc3e302119a804b
Jacob Garby <j.garby@icloud.com>
2017-06-08 18:23:17 +0100

    Done layout and CSS for register form

--- Source/Cordial/register/index.php
+++ Source/Cordial/register/index.php
@@ -10,6 +10,22 @@
     <link rel="stylesheet" href="../styles/register.css">
   </head>
   <body>
-
+    <div class="wrapper">
+      <div class="logo">
+        <img src="../assets/logobig-white.svg" />
+      </div>
+      <form autocomplete="off" method="post">
+        <legend>
+          New user
+        </legend>
+        <input type="text" placeholder="Choose a username" />
+        <br /><br />
+        <input type="password" name="password" placeholder="Type your password">
+        <br />
+        <input type="password" placeholder="Repeat your password" />
+        <br /><br />
+        <input type="submit" value="Create!" />
+      </form>
+    </div>
   </body>
 </html>
