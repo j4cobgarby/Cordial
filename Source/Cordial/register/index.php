a2f57d375f0f125059ce837f2a3cc9e028523832
Jacob Garby <j.garby@icloud.com>
2017-06-08 17:38:28 +0100

    Made files for register

---
+++ Source/Cordial/register/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php require '../creds.php'; ?>
+
+    <meta charset="utf-8">
+    <title>Cordial - register</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Overpass+Mono|Roboto|Roboto+Condensed" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/register.css">
+  </head>
+  <body>
+
+  </body>
+</html>
