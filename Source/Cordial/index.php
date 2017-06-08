9019a8e96e988b78b296dc8f590b59dd461d4c9f
Jacob Garby <j.garby@icloud.com>
2017-06-08 20:18:21 +0100

    Done many things, in the description
    - Completed registering
    - Taken the header into a subheader and rootheader
    - Set links to login page and register page

--- Source/Cordial/index.php
+++ Source/Cordial/index.php
@@ -38,37 +38,7 @@
     <script src="scripts/script.js" charset="utf-8"></script>
   </head>
   <body>
-    <div class="header-wrapper">
-      <img class="logo" src="assets/logobig-white.svg" onclick="location.href='?cat=all'" />
-
-      <span onclick="togglescroll();position == 'up' ? this.innerHTML = 'Categories' : this.innerHTML = 'User';"
-            class="label noselect">
-        Categories
-      </span>
-
-      <div class="scroller-wrapper">
-        <div id="panel" class="scroller-panel">
-          <a title="All" class="all" href="?cat=all"><b>all</b></a>
-          <a title="Software"    href="?cat=swar">swar</a>
-          <a title="Hardware"    href="?cat=hwar">hwar</a>
-          <a title="Game Dev"    href="?cat=gdev">gdev</a>
-          <a title="Web Dev"     href="?cat=wdev">wdev</a>
-          <a title="Memes"       href="?cat=meme">meme</a>
-          <a title="Photography" href="?cat=pics">pics</a>
-          <a title="Politics"    href="?cat=pols">pols</a>
-          <a title="Random"      href="?cat=rand">rand</a>
-          <a title="Meta"        href="?cat=meta">meta</a>
-
-          <br />
-
-          <a href="#">sign in</a>
-          <a href="#">register</a>
-          <a href="#">home</a>
-        </div>
-      </div>
-
-      <span onclick="location.href='compose'" class="hoverpointer compose">+</span>
-    </div>
+    <?php require 'rootheader.php'; ?>
 
     <div class="postview" id="postview">
       <?php
