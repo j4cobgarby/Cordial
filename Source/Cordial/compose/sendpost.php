f2728641264fd1764d0217ea44f0915c29c219a3
Jacob Garby <j.garby@icloud.com>
2017-06-23 15:17:36 +0100

    Changed sendpost.php so that it'll only happen if the right button is clicked

--- Source/Cordial/compose/sendpost.php
+++ Source/Cordial/compose/sendpost.php
@@ -1,24 +1,26 @@
 <?php
   require '../phpneed.php';
 
-  $sql = '
-    INSERT INTO `posts`
-      (`post_id`, `user_id`, `date_posted`, `category`, `title`, `content`, `likes`, `views`)
+  if (isset($_POST["sendpost"])) {
+    $sql = '
+      INSERT INTO `posts`
+        (`post_id`, `user_id`, `date_posted`, `category`, `title`, `content`, `likes`, `views`)
 
-    VALUES (
-      NULL,
-      '.$_SESSION["login-id"].',
-      "'.date("Y-m-d").'",
-      "' .addslashes($_POST["category"]) .'",
-      "' .addslashes($_POST["title"])    .'",
-      "' .addslashes($_POST["content"])  .'",
-      0, 0
-    );
-  ';
+      VALUES (
+        NULL,
+        '.$_SESSION["login-id"].',
+        "'.date("Y-m-d").'",
+        "' .addslashes($_POST["category"]) .'",
+        "' .addslashes($_POST["title"])    .'",
+        "' .addslashes($_POST["content"])  .'",
+        0, 0
+      );
+    ';
 
-  $connect = mysqli_connect($host_name, $user_name, $password, $database);
+    $connect = mysqli_connect($host_name, $user_name, $password, $database);
 
-  $result = mysqli_query($connect, $sql);
+    $result = mysqli_query($connect, $sql);
 
-  echo "<script>window.location.href='../'</script>";
+    echo "<script>window.location.href='../'</script>";
+  }
 ?>
