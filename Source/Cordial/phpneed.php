bec9a60df451672667ac08713d2539e8eb549f9b
Jacob Garby <j.garby@icloud.com>
2017-06-24 15:00:06 +0100

    Removed an unusued function and made notifications be sent when liking a post

--- Source/Cordial/phpneed.php
+++ Source/Cordial/phpneed.php
@@ -59,18 +59,6 @@
     mysqli_query($connect, $sql);
   }
 
-  // Likes or unlikes a post based on if it's already liked
-  function likePost($id, $host_name, $user_name, $password, $database) {
-    $connect = mysqli_connect($host_name, $user_name, $password, $database);
-    if (canLike($id, $host_name, $user_name, $password, $database) == true) {
-      $sql = 'UPDATE posts SET likes = likes + 1 WHERE post_id = '.$id;
-      mysqli_query($connect, $sql);
-      $set_liked = 'INSERT INTO user_liked_posts (user_id, post_id) VALUES ('.$_SESSION["login-id"].', '.$id.')';
-      mysqli_query($connect, $set_liked);
-      echo "<script>window.location.href='../post/?id=".$id."'</script>";
-    }
-  }
-
   if (isset($_SESSION["login-id"])) {
     $connect = mysqli_connect($host_name, $user_name, $password, $database);
 
